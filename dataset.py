28eb2f4b6217b658e25e7b9ed3a15cbcaad3e9be
yjhwang-cubox-ai <youngjun.hwang@cubox.ai>
2024-09-17 16:16:49 +0900

    feat: dataset

---
+++ dataset.py
@@ -0,0 +1,13 @@
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset
+
+class CustomDataset(Dataset):
+    def __init__(self, data):
+        self.data = data
+
+    def __len__(self):
+        return self.data.shape[0]
+
+    def __getitem__(self, idx):
+        return self.data.loc[idx, '원문'], self.data.loc[idx, '번역문']
